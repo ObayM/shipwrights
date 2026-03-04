2acae0b6dd3edf895cbdd7f8e09a9b404ea9eaee
Nullskulls <nullskulls@proton.me>
2026-03-05 01:36:41 +0200

    reformat feedback message

--- sw-bot/Source/msg_blocks.py
+++ sw-bot/Source/msg_blocks.py
@@ -4,11 +4,21 @@ def feedback_message(ticket_id):
 			"type": "section",
 			"text": {
 				"type": "mrkdwn",
-				"text": "*Hey there, fellow chef!* :wave-pikachu-2:\nIf you could give us 20 seconds of your time, we would love to know how we did in this ticket!"
+				"text": "*Hey there, fellow chef!* :wave-pikachu-2:\n"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "If you could give us 20 seconds of your time, we would love to know how we did in this ticket!"
 			},
 			"accessory": {
 				"type": "button",
-				"text": {"type": "plain_text", "text": "Submit Feedback"},
+				"text": {
+					"type": "plain_text",
+					"text": "Submit Feedback"
+				},
 				"style": "primary",
 				"value": str(ticket_id),
 				"action_id": "submit_feedback"
